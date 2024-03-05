f2fb089a5eed50bc40518d34522c01ffb96c05bc
Rishabh Raj <33702025+rshvraj@users.noreply.github.com>
2024-03-05 16:37:51 +0530

    Error handler added

--- server/index.js
+++ server/index.js
@@ -15,19 +15,16 @@ mongoose
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Connect to MongoDB
-
-// Middleware
-
-// Routes
-app.post("/api/register", (req, res) => {
-  const { name, email, password } = req.body;
-
-  const newUser = { name, email, password };
-
-  res.json({ message: "User registered successfully", user: newUser });
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
