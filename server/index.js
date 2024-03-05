70e93468507e0eff3de24484d950622df472d579
Rishabh Raj <33702025+rshvraj@users.noreply.github.com>
2024-03-05 16:27:55 +0530

    connected to db

--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
-const express = require("express");
-const mongoose = require("mongoose");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
